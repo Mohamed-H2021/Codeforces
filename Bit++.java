2ac0b1e064177210820e37f8fdf144c685a821f7
Mohamed-H2021 <63910466+Mohamed-H2021@users.noreply.github.com>
2020-11-19 17:38:02 +0200

    Update Bit++.java

--- Bit++.java
+++ Bit++.java
@@ -1,10 +1,5 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
-/**
- *
- * @author lenovo
- */
 public class Bit {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
