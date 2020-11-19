b5a9a4f01e4d7ec9e6443f5ce6e77c1bbd2d1dbc
Mohamed-H2021 <63910466+Mohamed-H2021@users.noreply.github.com>
2020-11-19 17:03:19 +0200

    Create Domino piling.java

---
+++ Domino piling.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Dominopiling {
+    public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    int m=input.nextInt();
+    int n=input.nextInt();
+    int k=(m/2)*(n/1);
+
+        System.out.println((n*m)/2);
+    }
+
+
+}
